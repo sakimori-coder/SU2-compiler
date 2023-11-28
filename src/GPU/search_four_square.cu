41b5f930bd1e5d6fb724018e0c1cff3cf85cf9b4
sakimori-coder <morisaki444@gmail.com>
2023-11-28 12:53:08 +0900

    add GPU

---
+++ src/GPU/search_four_square.cu
@@ -0,0 +1,154 @@
+#pragma once
+
+#include <bits/stdc++.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/sort.h>
+
+template<class RandomAccessIterator>
+using Pair_Iterator = std::pair<RandomAccessIterator, RandomAccessIterator>;
+
+template<class RandomAccessIterator>
+using Tuple_Iterator = std::tuple<RandomAccessIterator, RandomAccessIterator, RandomAccessIterator, RandomAccessIterator>;
+
+/*
+x^2 + y^2 = keyとなる要素を探索する. 
+*/
+template<class RandomAccessIterator, typename T>
+Pair_Iterator<RandomAccessIterator> search_two_square(
+                            RandomAccessIterator first_X, RandomAccessIterator last_X,
+                            RandomAccessIterator first_Y, RandomAccessIterator last_Y, 
+                            const T& key)
+{
+    for(auto it1 = first_X; it1 != last_X; it1++){
+        for(auto it2 = first_Y; it2 != last_Y; it2++){
+            if((*it1)*(*it1)+(*it2)*(*it2) == key){
+                return {it1, it2};
+            }
+        }
+    }
+
+    return {last_X, last_Y};
+}
+
+
+/*
+ソート済み配列A,Bからa_i + b_j = keyとなるi,jを探索する.
+*/
+template<class RandomAccessIterator, typename T>
+std::vector<Pair_Iterator<RandomAccessIterator>> two_points_technique(
+                            RandomAccessIterator first1, RandomAccessIterator last1,
+                            RandomAccessIterator first2, RandomAccessIterator last2,
+                            const T& key)
+{
+    std::vector<Pair_Iterator<RandomAccessIterator>> ret;
+    static const T zero = 0;
+    auto left = first1;
+    auto right = last2 - 1;
+    for(left = first1; left != last1; left++){
+        while(right != first2-1){
+            T diff = key - *left - *right;
+            if(diff == zero){
+                ret.push_back({left, right});
+            }
+            if(diff < zero) right--;
+            else break;
+        }
+    }
+
+    return ret;
+}
+
+
+
+/*
+x^2 + y^2 + z^2 + w^2 = keyとなる要素を探索する. 
+そのような要素があるときはそのiteratorを返す. 
+存在しなときは最後のiteratorを返す. 
+*/
+template<class RandomAccessIterator, typename T>
+std::vector<Tuple_Iterator<RandomAccessIterator>> search_four_square(
+                            RandomAccessIterator first_X, RandomAccessIterator last_X,
+                            RandomAccessIterator first_Y, RandomAccessIterator last_Y,
+                            RandomAccessIterator first_Z, RandomAccessIterator last_Z,
+                            RandomAccessIterator first_W, RandomAccessIterator last_W,
+                            const T& key)
+{
+    unsigned long N1 = std::distance(first_X, last_X);
+    unsigned long N2 = std::distance(first_Y, last_Y);
+    unsigned long N3 = std::distance(first_Z, last_Z);
+    unsigned long N4 = std::distance(first_W, last_W);
+
+
+    int i = 0;
+    std::vector<T> X_squared(N1);
+    for(auto it = first_X; it != last_X; it++){
+        X_squared[i] = (*it)*(*it);
+        i++;
+    }
+
+    i = 0;
+    std::vector<T> Y_squared(N2);
+    for(auto it = first_Y; it != last_Y; it++){
+        Y_squared[i] = (*it)*(*it);
+        i++;
+    }
+
+    i = 0;
+    std::vector<T> Z_squared(N3);
+    for(auto it = first_Z; it != last_Z; it++){
+        Z_squared[i] = (*it)*(*it);
+        i++;
+    }
+
+    i = 0;
+    std::vector<T> W_squared(N4);
+    for(auto it = first_W; it != last_W; it++){
+        W_squared[i] = (*it)*(*it);
+        i++;
+    }
+
+    // 事前にソートしておくと交換回数が減って後のソートが少しだけ高速になる
+    std::sort(X_squared.begin(), X_squared.end());
+    std::sort(Y_squared.begin(), Y_squared.end());
+    std::sort(Z_squared.begin(), Z_squared.end());
+    std::sort(W_squared.begin(), W_squared.end());
+
+    thrust::host_vector<T> XY(N1*N2), ZW(N3*N4);
+    thrust::device_vector<T> XY_device(N1*N2), ZW_device(N3*N4);
+
+    for(int i = 0; i < N1; i++){
+        for(int j = 0; j < N2; j++) XY[i*N2 + j] = X_squared[i] + Y_squared[j];
+    }
+    for(int i = 0; i < N3; i++){
+        for(int j = 0; j < N4; j++) ZW[i*N4 + j] = Z_squared[i] + W_squared[j];
+    } 
+
+    // メモリ転送 CPU -> GPU
+    thrust::copy(XY.begin(), XY.end(), XY_device.begin());
+    thrust::copy(ZW.begin(), ZW.end(), ZW_device.begin());
+    // GPU上でソート
+    // thrust::sort(XY_device.begin(), XY_device.end());
+    // thrust::sort(ZW_device.begin(), ZW_device.end());
+    // メモリ転送 GPU -> CPU
+    thrust::copy(XY_device.begin(), XY_device.end(), XY.begin());
+    thrust::copy(ZW_device.begin(), ZW_device.end(), ZW.begin());
+
+
+    auto solutions = two_points_technique(XY.begin(), XY.end(), ZW.begin(), ZW.end(), key);
+
+
+    std::vector<Tuple_Iterator<RandomAccessIterator>> ret;
+    // ret.push_back({first_X, first_Y, first_Z, first_W});
+    for(auto P : solutions){
+        auto left = P.first;
+        auto right = P.second;
+        
+        auto [x_ans, y_ans] = search_two_square(first_X, last_X, first_Y, last_Y, *left);
+        auto [z_ans, w_ans] = search_two_square(first_Z, last_Z, first_W, last_W, *right);
+
+        ret.push_back({x_ans, y_ans, z_ans, w_ans});
+    }
+
+    return ret;
+}
