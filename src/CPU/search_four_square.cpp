264b735e52d04f4a75d7d76dc0484332e298c3d8
morisaki <u748119d@ecs.osaka-u.ac.jp>
2023-11-29 14:35:58 +0900

    fix

--- src/CPU/search_four_square.cpp
+++ src/CPU/search_four_square.cpp
@@ -146,3 +146,84 @@ std::vector<Tuple_Iterator<RandomAccessIterator>> search_four_square(
 
     return ret;
 }
+
+
+
+
+template<class RandomAccessIterator, typename T>
+std::vector<Tuple_Iterator<RandomAccessIterator>> search_four_square_using_vector(
+                            RandomAccessIterator first_X, RandomAccessIterator last_X,
+                            RandomAccessIterator first_Y, RandomAccessIterator last_Y,
+                            RandomAccessIterator first_Z, RandomAccessIterator last_Z,
+                            RandomAccessIterator first_W, RandomAccessIterator last_W,
+                            const T& key)
+{
+    unsigned long N1 = std::distance(first_X, last_X);
+    unsigned long N2 = std::distance(first_Y, last_Y);
+    unsigned long N3 = std::distance(first_Z, last_Z);
+    unsigned long N4 = std::distance(first_W, last_W);
+
+
+    int i = 0;
+    std::vector<T> X_squared(N1);
+    for(auto it = first_X; it != last_X; it++){
+        X_squared[i] = (*it)*(*it);
+        i++;
+    }
+
+    i = 0;
+    std::vector<T> Y_squared(N2);
+    for(auto it = first_Y; it != last_Y; it++){
+        Y_squared[i] = (*it)*(*it);
+        i++;
+    }
+
+    i = 0;
+    std::vector<T> Z_squared(N3);
+    for(auto it = first_Z; it != last_Z; it++){
+        Z_squared[i] = (*it)*(*it);
+        i++;
+    }
+
+    i = 0;
+    std::vector<T> W_squared(N4);
+    for(auto it = first_W; it != last_W; it++){
+        W_squared[i] = (*it)*(*it);
+        i++;
+    }
+
+    // 事前にソートしておくと交換回数が減って後のソートが少しだけ高速になる
+    std::sort(X_squared.begin(), X_squared.end());
+    std::sort(Y_squared.begin(), Y_squared.end());
+    std::sort(Z_squared.begin(), Z_squared.end());
+    std::sort(W_squared.begin(), W_squared.end());
+
+    std::vector<T> XY(N1*N2), ZW(N3*N4);
+
+    for(int i = 0; i < N1; i++){
+        for(int j = 0; j < N2; j++) XY[i*N2 + j] = X_squared[i] + Y_squared[j];
+    }
+    for(int i = 0; i < N3; i++){
+        for(int j = 0; j < N4; j++) ZW[i*N4 + j] = Z_squared[i] + W_squared[j];
+    } 
+
+    std::sort(XY.begin(), XY.end());
+    std::sort(ZW.begin(), ZW.end());
+
+
+    auto solutions = two_points_technique(XY.begin(), XY.end(), ZW.begin(), ZW.end(), key);
+
+
+    std::vector<Tuple_Iterator<RandomAccessIterator>> ret;
+    for(auto P : solutions){
+        auto left = P.first;
+        auto right = P.second;
+        
+        auto [x_ans, y_ans] = search_two_square(first_X, last_X, first_Y, last_Y, *left);
+        auto [z_ans, w_ans] = search_two_square(first_Z, last_Z, first_W, last_W, *right);
+
+        ret.push_back({x_ans, y_ans, z_ans, w_ans});
+    }
+
+    return ret;
+}
--- src/plot/plot.py
+++ src/plot/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import japanize_matplotlib
 
 
 x = []
@@ -23,9 +24,10 @@ y_ideal = [3*np.log2(1/eps) for eps in x_ideal]
 
 clr = plt.cm.Purples(0.9)
 fig, ax = plt.subplots()
-ax.scatter(x, mean, label="mean")
-ax.fill_between(x, MIN, MAX, alpha=0.3, edgecolor=clr, facecolor=clr, label="MIN-MAX range")
+ax.scatter(x, mean, label="平均値", color="b")
+ax.fill_between(x, MIN, MAX, alpha=0.3, edgecolor=clr, facecolor=clr, label="最小値-最大値")
 ax.plot(x_ideal, y_ideal, "--", color="r", label="$3log_{2}(1/\epsilon)$")
+ax.grid()
 
 ax.set_xscale("log")
 x.reverse()
@@ -33,9 +35,10 @@ ax.set_xticks(x)
 ax.invert_xaxis()
 ax.set_xlim(0.7, 0.5*1e-9)
 
-ax.set_xlabel("Approximation accuracy ε")
-ax.set_ylabel("T-count")
+ax.set_xlabel("Approximation accuracy ε", fontsize=18)
+ax.set_ylabel("T-count", fontsize=18)
+ax.set_title("ランダムユニタリの$\epsilon$近似に必要なTゲート数", fontsize=18)
 
-ax.legend()
+ax.legend(loc="upper left", fontsize=15)
 
-plt.savefig("T_count.png")
+plt.savefig("T_count.png", dpi=500)
