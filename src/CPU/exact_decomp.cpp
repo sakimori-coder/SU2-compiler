4eb976df915220810b2a6a595f74c3903e34a879
morisaki <u748119d@ecs.osaka-u.ac.jp>
2024-01-09 12:24:39 +0900

    add exact

--- src/CPU/exact_decomp.cpp
+++ src/CPU/exact_decomp.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 #include "Rings.cpp"
 
+
+namespace exact
+{
+    using matrix2cd = std::vector<std::vector<std::complex<double>>>;
+    double sqrt2 = std::sqrt(2.0);
+    double isqrt2 = 1. / sqrt2;
+}
+
 template<typename T>
 std::string exact_decomposition(ZOmega<T> u, ZOmega<T> t, int k){
     std::string S_out = "";
